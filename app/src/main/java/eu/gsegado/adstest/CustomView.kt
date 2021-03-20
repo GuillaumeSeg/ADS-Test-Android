fe8275389700043b3938ce034462e9c54ac38fa3
GuillaumeS <guillaume.segado@gmail.com>
2021-03-20 19:05:59 +0100

    CM - add behaviorSubject for event and observe it

--- app/src/main/java/eu/gsegado/adstest/CustomView.kt
+++ app/src/main/java/eu/gsegado/adstest/CustomView.kt
@@ -2,15 +2,54 @@ package eu.gsegado.adstest
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
+import android.widget.ArrayAdapter
 import androidx.constraintlayout.widget.ConstraintLayout
 import eu.gsegado.adstest.databinding.ItemViewBinding
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.Calendar
 
 class CustomView(context: Context, attrs: AttributeSet): ConstraintLayout(context, attrs) {
     private var binding: ItemViewBinding
 
+    private val items : MutableList<String> = mutableListOf()
+    private lateinit var itemsAdapter: ArrayAdapter<String>
+
     init {
         val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         binding = ItemViewBinding.inflate(inflater, this, true)
+
+        itemsAdapter =  ArrayAdapter<String>(binding.root.context, R.layout.item_list, items)
+        binding.list.adapter = itemsAdapter
+
+        binding.button.setOnClickListener {
+            val mainActivity: MainActivity = context as MainActivity
+
+            mainActivity.eventSubject.onNext(2)
+        }
+
+        // Observer
+        // TODO - close clean dispose alert !
+        (context as MainActivity).eventSubject
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onNext = {
+                    formatTimeCapture()
+                },
+                onError = {
+                    Log.e("FragmentMother", "Error while receiving an event")
+                }
+            )
+    }
+
+    private fun formatTimeCapture() {
+        val time = Calendar.getInstance().timeInMillis
+        items.add("${items.size+1} - $time")
+
+        itemsAdapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/eu/gsegado/adstest/MainActivity.kt
+++ app/src/main/java/eu/gsegado/adstest/MainActivity.kt
@@ -1,14 +1,22 @@
 package eu.gsegado.adstest
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
 import eu.gsegado.adstest.databinding.ActivityMainBinding
 import eu.gsegado.adstest.databinding.DrawerHeaderBinding
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 import java.util.Calendar
 
 class MainActivity : FragmentActivity() {
+
+    val eventSubject: BehaviorSubject<Int> = BehaviorSubject.create()
+
     private val items : MutableList<String> = mutableListOf()
     private val itemsAdapter : ArrayAdapter<String> by lazy { ArrayAdapter<String>(this, R.layout.item_list, items) }
 
@@ -21,16 +29,33 @@ class MainActivity : FragmentActivity() {
         // Navigation Drawer
         val navHeaderBinding: DrawerHeaderBinding = DataBindingUtil.inflate(layoutInflater, R.layout.drawer_header, binding.navigationDrawer, true)
         navHeaderBinding.button.setOnClickListener {
-            val time = Calendar.getInstance().timeInMillis
-            items.add("${items.size+1} - $time")
-
-            itemsAdapter.notifyDataSetChanged()
+            eventSubject.onNext(1)
         }
 
         // Adapter
         navHeaderBinding.list.adapter = itemsAdapter
         navHeaderBinding.list.divider = null
 
+        // Observer
+        // TODO - close clean dispose alert !
+        eventSubject
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onNext = {
+                    formatTimeCapture()
+                },
+                onError = {
+                    Log.e("MainActivity", "Error while receiving an event")
+                }
+            )
+    }
+
+    private fun formatTimeCapture() {
+        val time = Calendar.getInstance().timeInMillis
+        items.add("${items.size+1} - $time")
+
+        itemsAdapter.notifyDataSetChanged()
     }
 
 }
