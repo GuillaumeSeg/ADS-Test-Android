0a666cd7736254865ab479ae7670d792db5793fe
GuillaumeS <guillaume.segado@gmail.com>
2021-03-20 12:18:57 +0100

    NF - ArrayAdapter and clicklistener on Drawer

--- app/src/main/java/eu/gsegado/adstest/MainActivity.kt
+++ app/src/main/java/eu/gsegado/adstest/MainActivity.kt
@@ -1,24 +1,36 @@
 package eu.gsegado.adstest
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
+import eu.gsegado.adstest.databinding.ActivityMainBinding
+import eu.gsegado.adstest.databinding.DrawerHeaderBinding
+import java.util.Calendar
 
 class MainActivity : FragmentActivity() {
+    private val items : MutableList<String> = mutableListOf()
+    private val itemsAdapter : ArrayAdapter<String> by lazy { ArrayAdapter<String>(this, R.layout.item_list, items) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        // Inflate binding
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Navigation Drawer
+        val navHeaderBinding: DrawerHeaderBinding = DataBindingUtil.inflate(layoutInflater, R.layout.drawer_header, binding.navigationDrawer, true)
+        navHeaderBinding.button.setOnClickListener {
+            val time = Calendar.getInstance().timeInMillis
+            items.add("${items.size+1} - $time")
+
+            itemsAdapter.notifyDataSetChanged()
+        }
+
+        // Adapter
+        navHeaderBinding.list.adapter = itemsAdapter
+        navHeaderBinding.list.divider = null
 
     }
-
-    /*private fun addMotherFragment(id: Int) {
-        val newFragment = MotherFragment()
-        val transaction : FragmentTransaction = supportFragmentManager.beginTransaction()
-
-        transaction.replace(id, newFragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }*/
 
 }
